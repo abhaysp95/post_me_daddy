644b379b8adef142d643abd2d5c5f7099a6a02a0
abhay <abhaysp95@protonmail.com>
2023-03-16 12:32:02 +0530

    feat: using optional, default and specifying type with orm entity

--- post_me_server/src/entities/Post.ts
+++ post_me_server/src/entities/Post.ts
@@ -3,15 +3,17 @@ import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
 @Entity()
 export class Post {
 
+	[OptionalProps]?: 'createdAt' | 'updatedAt'
+
 	@PrimaryKey()
 	id!: number;
 
-	@Property()
+	@Property({ type: "date" })
 	createdAt: Date =  new Date();
 
-	@Property({ onUpdate: () => new Date() })
+	@Property({ type: "date", onUpdate: () => new Date() })
 	updatedAt: Date = new Date();
 
-	@Property()
+	@Property(/* { default: 'name' } */)
 	title!: String;
 }
