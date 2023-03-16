593329ffc1c82bf758fac185eeecdbbbaa669f21
abhay <abhaysp95@protonmail.com>
2023-03-16 16:39:00 +0530

    feat: using expressMiddleware to attach apollo server to express server

--- post_me_server/dist/index.js
+++ post_me_server/dist/index.js
@@ -3,24 +3,37 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const server_1 = require("@apollo/server");
+const express4_1 = require("@apollo/server/express4");
+const drainHttpServer_1 = require("@apollo/server/plugin/drainHttpServer");
 const core_1 = require("@mikro-orm/core");
-const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const body_parser_1 = require("body-parser");
+const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const type_graphql_1 = require("type-graphql");
+const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const hello_1 = require("./resolvers/hello");
 const main = async () => {
     const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
     orm.getMigrator().up();
     const app = (0, express_1.default)();
+    const httpServer = http_1.default.createServer(app);
+    const apolloServer = new server_1.ApolloServer({
+        schema: await (0, type_graphql_1.buildSchema)({
+            resolvers: [hello_1.HelloResolver],
+            validate: false,
+        }),
+        plugins: [(0, drainHttpServer_1.ApolloServerPluginDrainHttpServer)({ httpServer })]
+    });
+    await apolloServer.start();
+    app.use('/graphql', (0, cors_1.default)(), (0, body_parser_1.json)(), (0, express4_1.expressMiddleware)(apolloServer, { context: async ({ req }) => ({ token: req.headers.token }) }));
     app.get('/:name', (request, response) => {
-        response.write(`Hello ${request.params.name}`, () => {
-            console.log(`Sent hello to ${request.params.name}`);
-        });
+        response.write(`Hello ${request.params.name}`, () => { console.log(`Sent hello to ${request.params.name}`); });
         response.end().status(200);
     });
-    app.listen(4000, () => {
-        console.log("server started at 4000");
-    });
+    await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+    console.log("Server running at: http://localhost:4000");
 };
-main().catch(err => {
-    console.error(err);
-});
+main().catch(err => { console.error(err); });
 //# sourceMappingURL=index.js.map
--- post_me_server/src/index.ts
+++ post_me_server/src/index.ts
@@ -1,39 +1,83 @@
-import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
+import {ApolloServer} from '@apollo/server';
+import {expressMiddleware} from '@apollo/server/express4';
+import {
+  ApolloServerPluginDrainHttpServer
+} from '@apollo/server/plugin/drainHttpServer';
+// import {startStandaloneServer} from '@apollo/server/standalone';
+import {MikroORM} from "@mikro-orm/core";
+import {json} from "body-parser";
+import cors from "cors";
+import express from "express";
+import http from "http";
+import {buildSchema} from 'type-graphql';
+
+import {__prod__} from "./constants";
 // import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
-import express from "express";
+import {HelloResolver} from "./resolvers/hello";
 
-const main = async () => {
-	const orm = await MikroORM.init(mikroConfig);
-	orm.getMigrator().up()
+const main =
+    async () => {
+  const orm = await MikroORM.init(mikroConfig);
+  orm.getMigrator().up()
 
-	/* const generator = orm.getSchemaGenerator();
-	await generator.createSchema(); */
+  /* const generator = orm.getSchemaGenerator();
+  await generator.createSchema(); */
 
-	// const em = orm.em.fork()
-	/* const post = em.create(Post, {title: 'my first post'}); // an instance of post
-	await em.persistAndFlush(post); */
+  // const em = orm.em.fork()
+  /* const post = em.create(Post, {title: 'my first post'}); // an instance of
+  post await em.persistAndFlush(post); */
 
-	/* const posts = await em.find(Post, {});
-	console.log(posts); */
+  /* const posts = await em.find(Post, {});
+  console.log(posts); */
 
-   const app = express();
-   /* app.get('/:name', (request, response) => {
-	   response.send(`Hello ${request.params.name}`).status(200);
-   }) */
-   app.get('/:name', (request, response) => {
-	   response.write(`Hello ${request.params.name}`, () => {
-		   console.log(`Sent hello to ${request.params.name}`);
-	   })
-	   response.end().status(200); // send() can be called once, but write() many times, it needs end() to tell the end() else it's blocking
-   })
-   app.listen(4000, () => {
-	   console.log("server started at 4000")
-   })
+  const app = express();
+  const httpServer = http.createServer(app);
+  /* app.get('/:name', (request, response) => {
+          response.send(`Hello ${request.params.name}`).status(200);
+  }) */
 
+  const apolloServer = new ApolloServer({
+    schema : await buildSchema({
+      resolvers : [ HelloResolver ],
+      validate : false,
+    }),
+    // to drain the "httpServer" for graceful shutdown of the server
+    plugins : [ ApolloServerPluginDrainHttpServer({httpServer}) ]
+  });
+  await apolloServer.start();
+
+  // using a middleware (order of app.use() for middleware matters)
+  app.use('/graphql', cors<cors.CorsRequest>(), json(),
+          expressMiddleware(
+              apolloServer,
+              {context : async ({req}) => ({token : req.headers.token})}));
+
+  app.get('/:name',
+          (request, response) => {
+            response.write(
+                `Hello ${request.params.name}`,
+                () => { console.log(`Sent hello to ${request.params.name}`); })
+            response.end().status(
+                200); // send() can be called once, but write() many times, it
+                      // needs end() to tell the end() else it's blocking
+          })
+      /* app.listen(4000, () => {
+              console.log("server started at 4000")
+      }) */
+
+      /**
+       * using "startStandaloneServer"
+       * creates and express app
+       * installs ApolloServer as middleware
+       * prepares your app to handle incoming request
+       */
+      /* const { url } = await startStandaloneServer(apolloServer, {
+              listen: { port: 4000 },
+      }); */
+      await new Promise<void>((resolve) =>
+                                  httpServer.listen({port : 4000}, resolve));
+  console.log("Server running at: http://localhost:4000");
 }
 
-main().catch(err => {
-	console.error(err);
-});
+main().catch(err => { console.error(err); });
