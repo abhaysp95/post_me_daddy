bc230796faab5e325304b57dba5185c827f2848d
abhay <abhaysp95@protonmail.com>
2023-03-16 13:37:09 +0530

    feat: updates with new packages and setup a basic server

--- post_me_server/dist/index.js
+++ post_me_server/dist/index.js
@@ -4,14 +4,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@mikro-orm/core");
-const Post_1 = require("./entities/Post");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const express_1 = __importDefault(require("express"));
 const main = async () => {
     const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
     orm.getMigrator().up();
-    const em = orm.em.fork();
-    const posts = await em.find(Post_1.Post, {});
-    console.log(posts);
+    const app = (0, express_1.default)();
+    app.get('/:name', (request, response) => {
+        response.write(`Hello ${request.params.name}`, () => {
+            console.log(`Sent hello to ${request.params.name}`);
+        });
+        response.end().status(200);
+    });
+    app.listen(4000, () => {
+        console.log("server started at 4000");
+    });
 };
 main().catch(err => {
     console.error(err);
--- post_me_server/src/index.ts
+++ post_me_server/src/index.ts
@@ -1,7 +1,8 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
+import express from "express";
 
 const main = async () => {
 	const orm = await MikroORM.init(mikroConfig);
@@ -10,13 +11,26 @@ const main = async () => {
 	/* const generator = orm.getSchemaGenerator();
 	await generator.createSchema(); */
 
-	const em = orm.em.fork()
+	// const em = orm.em.fork()
 	/* const post = em.create(Post, {title: 'my first post'}); // an instance of post
 	await em.persistAndFlush(post); */
 
-	const posts = await em.find(Post, {});
-	console.log(posts);
+	/* const posts = await em.find(Post, {});
+	console.log(posts); */
 
+   const app = express();
+   /* app.get('/:name', (request, response) => {
+	   response.send(`Hello ${request.params.name}`).status(200);
+   }) */
+   app.get('/:name', (request, response) => {
+	   response.write(`Hello ${request.params.name}`, () => {
+		   console.log(`Sent hello to ${request.params.name}`);
+	   })
+	   response.end().status(200); // send() can be called once, but write() many times, it needs end() to tell the end() else it's blocking
+   })
+   app.listen(4000, () => {
+	   console.log("server started at 4000")
+   })
 
 }
 
