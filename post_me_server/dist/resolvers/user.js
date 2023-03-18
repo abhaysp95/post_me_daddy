22185d62b32f36a36d184daef755d649f4a7c531
abhay <abhaysp95@protonmail.com>
2023-03-18 19:12:43 +0530

    feat: logic for proper response with good error message

--- post_me_server/dist/resolvers/user.js
+++ post_me_server/dist/resolvers/user.js
@@ -16,9 +16,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.UserResolver = void 0;
-const User_1 = require("../entities/User");
-const type_graphql_1 = require("type-graphql");
 const argon2_1 = __importDefault(require("argon2"));
+const type_graphql_1 = require("type-graphql");
+const User_1 = require("../entities/User");
 let UsernamePasswordInput = class UsernamePasswordInput {
 };
 __decorate([
@@ -32,22 +32,109 @@ __decorate([
 UsernamePasswordInput = __decorate([
     (0, type_graphql_1.InputType)()
 ], UsernamePasswordInput);
+let FieldError = class FieldError {
+};
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], FieldError.prototype, "field", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], FieldError.prototype, "message", void 0);
+FieldError = __decorate([
+    (0, type_graphql_1.ObjectType)()
+], FieldError);
+let UserResponse = class UserResponse {
+};
+__decorate([
+    (0, type_graphql_1.Field)(() => [FieldError], { nullable: true }),
+    __metadata("design:type", Array)
+], UserResponse.prototype, "errors", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(() => User_1.User, { nullable: true }),
+    __metadata("design:type", User_1.User)
+], UserResponse.prototype, "user", void 0);
+UserResponse = __decorate([
+    (0, type_graphql_1.ObjectType)()
+], UserResponse);
 let UserResolver = class UserResolver {
     async register(options, { em }) {
-        const hashedPassword = await argon2_1.default.hash(options.password);
-        const user = em.create(User_1.User, { username: options.username, password: hashedPassword });
+        if (options.username.length <= 2) {
+            return {
+                errors: [{
+                        field: 'username',
+                        message: 'Provided username with length > 2',
+                    }]
+            };
+        }
+        if (options.password.length <= 8) {
+            return {
+                errors: [{
+                        field: 'username',
+                        message: 'Provided password with length > 4',
+                    }]
+            };
+        }
+        let user = await em.findOne(User_1.User, { username: options.username });
+        if (user) {
+            return {
+                errors: [{
+                        field: 'username',
+                        message: 'Username already registered',
+                    }]
+            };
+        }
+        const hashedPassword = await argon2_1.default.hash(options.password, {
+            type: argon2_1.default.argon2id,
+            hashLength: 64,
+            memoryCost: 2 ** 16,
+            timeCost: 5,
+        });
+        user = em.create(User_1.User, { username: options.username, password: hashedPassword });
         await em.persistAndFlush(user);
-        return user;
+        return { user };
+    }
+    async login(options, { em }) {
+        const user = await em.findOne(User_1.User, { username: options.username });
+        if (!user) {
+            return {
+                errors: [{
+                        field: 'username',
+                        message: "Couldn't find provided username",
+                    }],
+            };
+        }
+        const hashPassword = await argon2_1.default.verify(user.password, options.password);
+        if (!hashPassword) {
+            return {
+                errors: [{
+                        field: 'password',
+                        message: 'Incorrect password provided',
+                    }]
+            };
+        }
+        return {
+            user,
+        };
     }
 };
 __decorate([
-    (0, type_graphql_1.Mutation)(() => User_1.User),
+    (0, type_graphql_1.Mutation)(() => UserResponse),
     __param(0, (0, type_graphql_1.Arg)("options")),
     __param(1, (0, type_graphql_1.Ctx)()),
     __metadata("design:type", Function),
     __metadata("design:paramtypes", [UsernamePasswordInput, Object]),
     __metadata("design:returntype", Promise)
 ], UserResolver.prototype, "register", null);
+__decorate([
+    (0, type_graphql_1.Mutation)(() => UserResponse, { nullable: true }),
+    __param(0, (0, type_graphql_1.Arg)("options")),
+    __param(1, (0, type_graphql_1.Ctx)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [UsernamePasswordInput, Object]),
+    __metadata("design:returntype", Promise)
+], UserResolver.prototype, "login", null);
 UserResolver = __decorate([
     (0, type_graphql_1.Resolver)()
 ], UserResolver);
--- post_me_server/src/resolvers/user.ts
+++ post_me_server/src/resolvers/user.ts
@@ -1,17 +1,39 @@
-import {User} from "../entities/User";
-import {MyContext} from "src/types";
-import {Arg, Ctx, Field, InputType, Mutation, Resolver} from "type-graphql";
 import argon2 from "argon2";
+import {MyContext} from "src/types";
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Resolver
+} from "type-graphql";
+
+import {User} from "../entities/User";
 
 @InputType()
 class UsernamePasswordInput {
 
-	@Field()
-	username!: string;
+  @Field() username!: string;
 
-	@Field()
-	password!: string;
+  @Field() password!: string;
+}
 
+@ObjectType()
+class FieldError {
+
+  @Field() field: string;
+
+  @Field() message: string;
+}
+
+@ObjectType()
+class UserResponse {
+
+  @Field(() => [FieldError], {nullable : true}) errors?: FieldError[];
+
+  @Field(() => User, {nullable : true}) user?: User;
 }
 
 @Resolver()
@@ -26,12 +48,72 @@ export class UserResolver {
                            // ...
   } */
 
- @Mutation(() => User)
- async register(@Arg("options") options: UsernamePasswordInput, @Ctx() { em }: MyContext): Promise<User> {
-	 const hashedPassword = await argon2.hash(options.password);
-	 const user = em.create(User, { username: options.username, password: hashedPassword });
-	 await em.persistAndFlush(user);
-	 return user;
- }
+  @Mutation(() => UserResponse)
+  async register(@Arg("options") options: UsernamePasswordInput,
+                 @Ctx() { em }: MyContext): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [ {
+          field : 'username',
+          message : 'Provided username with length > 2',
+        } ]
+      }
+    }
+    if (options.password.length <= 8) {
+      return {
+        errors: [ {
+          field : 'username',
+          message : 'Provided password with length > 4',
+        } ]
+      }
+    }
+	let user = await em.findOne(User, { username: options.username });
+	if (user) {
+		return {
+			errors: [ {
+				field: 'username',
+				message: 'Username already registered',
+			} ]
+		}
+	}
+    const hashedPassword = await argon2.hash(options.password, {
+      type : argon2.argon2id, // hybrid one
+      hashLength : 64,
+      memoryCost : 2 ** 16,
+      timeCost : 5,
+      // no need to worry about salt, as this module already generates good
+      // random salt and stores it,
+      // https://github.com/ranisalt/node-argon2/issues/76#issuecomment-291553840
+    });
+    user = em.create(
+        User, {username : options.username, password : hashedPassword});
+    await em.persistAndFlush(user);
+    return {user};
+  }
 
+  @Mutation(() => UserResponse, {nullable : true})
+  async login(@Arg("options") options: UsernamePasswordInput,
+              @Ctx() { em }: MyContext): Promise<UserResponse|null> {
+    const user = await em.findOne(User, {username : options.username});
+    if (!user) {
+      return {
+        errors: [ {
+          field : 'username',
+          message : "Couldn't find provided username",
+        } ],
+      }
+    }
+    const hashPassword = await argon2.verify(user.password, options.password);
+    if (!hashPassword) {
+      return {
+        errors : [ {
+          field : 'password',
+          message : 'Incorrect password provided',
+        } ]
+      };
+    }
+    return {
+      user,
+    };
+  }
 }
